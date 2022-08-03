56bacf1aca092646199a9162ceae95e3a787e7db
shadow-monarch <invicibleshadowmonarch@gmail.com>
2022-08-03 18:29:17 +0530

    re-initialization

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="app">
+    <mainComponent />
   </div>
 </template>
 <script>
+import mainComponent from './components/mainComponent.vue'
+import formComponent from './components/formComponent.vue'
 export default {
   name: 'App',
   components: {
+    mainComponent,
+    formComponent
   }
 }
 </script>
